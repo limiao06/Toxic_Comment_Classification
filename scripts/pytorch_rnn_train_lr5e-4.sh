de7789a3f7ca347a89026098dc23bc7cda4cc45f
limiao06 <limiaogg@126.com>
2018-01-18 09:03:06 +0800

    add some scripts

---
+++ scripts/pytorch_rnn_train_lr5e-4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# the script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${DIR}/../"
+
+
+python ToxicPytorch/main.py --model rnn --save_path model/pytorch_rnn_lr5e-4_model --log_every 10 --dev_every 0 --gpu 1 --lr 0.0005
