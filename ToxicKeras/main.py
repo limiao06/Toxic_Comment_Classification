58fb4fe0a827b7484cfcebf674f601d6c18d4dc3
limiao06 <limiaogg@126.com>
2018-02-26 17:41:17 -0800

    change the project structure, add model save and reload

--- ToxicKeras/main.py
+++ ToxicKeras/main.py
@@ -1,22 +1,5 @@
-import os
 from argparse import ArgumentParser
 import time
-
-
-import numpy as np
-np.random.seed(42)
-import pandas as pd
-
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score
-
-from keras.preprocessing import text, sequence
-from keras.callbacks import Callback
-from keras import backend as K
-
-from global_variables import DATA_DIR, NB_OUTPUT_CLASSES, OUTPUT_LABELS, EMBEDDING_DIR
-from utils import RocAucEvaluation
-import models
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -27,27 +10,21 @@ os.environ['OMP_NUM_THREADS'] = '4'
 
 def get_args():
     parser = ArgumentParser(description='Toxic model')
-    parser.add_argument('--epochs', type=int, default=5)
-    parser.add_argument('--batch_size', type=int, default=128)
-    parser.add_argument('--maxlen', type=int, default=100)
-    parser.add_argument('--vocab_size', type=int, default=30000)
-    parser.add_argument('--embedding_size', type=int, default=300)
-    parser.add_argument('--hidden_size', type=int, default=80)
-    parser.add_argument('--lr_init', type=float, default=.001)
-    parser.add_argument('--lr_fin', type=float, default=.0005)
-    parser.add_argument('--log_every', type=int, default=1)
-    parser.add_argument('--dev_every', type=int, default=300)
-    parser.add_argument('--save_every', type=int, default=300)
-    parser.add_argument('--dropout_rate', type=int, default=0.2)
-    parser.add_argument('--preserve-case', action='store_false', dest='lower')
-    parser.add_argument('--projection', action='store_true', dest='projection')
-    parser.add_argument('--gpu', type=int, default=0)
-    parser.add_argument('--save_path', type=str, default='model')
-    parser.add_argument('--resume_snapshot', type=str, default='')
-    parser.add_argument('--mode', type=str, default='train', help='mode: [train, test]')
+    parser.add_argument('--epochs', type=int, default=5, help='default=5')
+    parser.add_argument('--batch_size', type=int, default=128, help='default=128')
+    parser.add_argument('--maxlen', type=int, default=100, help='default=100')
+    parser.add_argument('--vocab_size', type=int, default=30000, help='default=30000')
+    parser.add_argument('--embedding_size', type=int, default=300, help='default=300, [50, 100, 200, 300]')
+    parser.add_argument('--hidden_size', type=int, default=80, help='default=80')
+    parser.add_argument('--lr_init', type=float, default=.001, help='default=0.001')
+    parser.add_argument('--lr_fin', type=float, default=.0005, help='default=0.0005')
+    parser.add_argument('--dropout_rate', type=int, default=0.2, help='default=0.2')
+    parser.add_argument('--gpu', type=int, default=1)
+    parser.add_argument('--save_path', type=str, default='', help='out model path')
     parser.add_argument('--cell', type=str, default='LSTM', help='rnn cell: [LSTM, GRU]')
     parser.add_argument('--output', type=str, default='', help='The result output path for test mode')
-
+    parser.add_argument('--monitor', type=str, default='val_acc', help='The monitor for model saving and earlystopping: [val_acc, val_loss]')
+    parser.add_argument('--patience', type=int, default=3, help='The patience for earlystopping, default=3')
     parser.add_argument('--model', type=str, default='pooled_gru', help='model: [pooled_gru]')
 
     # cnn parameter
@@ -59,66 +36,46 @@ def get_args():
 
 def main():
     args = get_args()
-    EMBEDDING_FILE = os.path.join(EMBEDDING_DIR, 'glove.6B.%dd.txt' %(args.embedding_size))
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
 
-    print("Loading data ... ")
-    os.path.join(DATA_DIR, 'train.csv')
-    train = pd.read_csv(os.path.join(DATA_DIR, 'train.csv'))
-    test = pd.read_csv(os.path.join(DATA_DIR, 'test.csv'))
-    submission = pd.read_csv(os.path.join(DATA_DIR, 'sample_submission.csv'))
+    if not args.save_path or args.output:
+        raise Exception("--save_path and --output must be set!")
 
-    X_train = train["comment_text"].fillna("fillna").values
-    y_train = train[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]].values
-    X_test = test["comment_text"].fillna("fillna").values
+    from utils import data_loader, RocAucEvaluation, training_callbacks
+    import models
+    from keras import backend as K
 
-
-    max_features = args.vocab_size
-    maxlen = args.maxlen
-    embed_size = args.embedding_size
-
-    tokenizer = text.Tokenizer(num_words=max_features)
-    tokenizer.fit_on_texts(list(X_train) + list(X_test))
-    X_train = tokenizer.texts_to_sequences(X_train)
-    X_test = tokenizer.texts_to_sequences(X_test)
-    x_train = sequence.pad_sequences(X_train, maxlen=maxlen)
-    x_test = sequence.pad_sequences(X_test, maxlen=maxlen)
-
-    print("Loading word embedding ... ")
-    def get_coefs(word, *arr): return word, np.asarray(arr, dtype='float32')
-    embeddings_index = dict(get_coefs(*o.strip().split()) for o in open(EMBEDDING_FILE))
-
-    all_embs = np.stack(embeddings_index.values())
-    emb_mean, emb_std = all_embs.mean(), all_embs.std()
-
-    word_index = tokenizer.word_index
-    nb_words = min(max_features, len(word_index))
-    embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
-    for word, i in word_index.items():
-        if i >= max_features: continue
-        embedding_vector = embeddings_index.get(word)
-        if embedding_vector is not None: embedding_matrix[i] = embedding_vector
-
+    # load data and embedding
+    X_tra, X_val, y_tra, y_val, embedding_matrix, nb_words = data_loader(args)
     args.embedding_matrix = embedding_matrix
     args.vocab_size = nb_words
 
+    # build model
     if args.model == 'pooled_gru':
         model = models.pooled_gru(args)
 
-
-    [X_tra, X_val, y_tra, y_val] = train_test_split(x_train, y_train, train_size=0.95, random_state=233)
-    RocAuc = RocAucEvaluation(validation_data=(X_val, y_val), interval=1)
-
+    # lr
     exp_decay = lambda init, fin, steps: (init/fin)**(1/(steps-1)) - 1
     steps = int(len(X_tra)/args.batch_size) * args.epochs
-
     lr_decay = exp_decay(args.lr_init, args.lr_fin, steps)
     K.set_value(model.optimizer.lr, args.lr_init)
     K.set_value(model.optimizer.decay, lr_decay)
 
+    # init callbacks
+    train_callbacks = training_callbacks(args.save_path, patience=args.patience, monitor=args.monitor)
+    RocAuc = RocAucEvaluation(validation_data=(X_val, y_val), interval=1)
+    train_callbacks.append(RocAuc)
+
+    # train
     hist = model.fit(X_tra, y_tra, batch_size=args.batch_size, epochs=args.epochs, validation_data=(X_val, y_val),
-                     callbacks=[RocAuc], verbose=1)
+                     callbacks=train_callbacks, verbose=1)
 
+    # load model from best weights
+    sleep(1)
+    print("Reload model from %s" %(args.save_path))
+    model.load_weights(args.save_path, by_name=False)
 
+    # predict
     y_pred = model.predict(x_test, batch_size=1024, verbose=1)
     submission[OUTPUT_LABELS] = y_pred
     submission.to_csv(args.output, index=False)
--- ToxicKeras/utils.py
+++ ToxicKeras/utils.py
@@ -1,5 +1,62 @@
-from keras.callbacks import Callback
+import os
+import numpy as np
+np.random.seed(42)
+import pandas as pd
+
+from global_variables import DATA_DIR, NB_OUTPUT_CLASSES, OUTPUT_LABELS, EMBEDDING_DIR
+
+from keras.preprocessing import text, sequence
+from keras import backend as K
+from keras.callbacks import Callback, ModelCheckpoint, EarlyStopping, CSVLogger
+
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
+
+
+
+def data_loader(args):
+    EMBEDDING_FILE = os.path.join(EMBEDDING_DIR, 'glove.6B.%dd.txt' %(args.embedding_size))
+
+    print("Loading data ... ")
+    os.path.join(DATA_DIR, 'train.csv')
+    train = pd.read_csv(os.path.join(DATA_DIR, 'train.csv'))
+    test = pd.read_csv(os.path.join(DATA_DIR, 'test.csv'))
+    submission = pd.read_csv(os.path.join(DATA_DIR, 'sample_submission.csv'))
+
+    X_train = train["comment_text"].fillna("fillna").values
+    y_train = train[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]].values
+    X_test = test["comment_text"].fillna("fillna").values
+
+
+    max_features = args.vocab_size
+    maxlen = args.maxlen
+    embed_size = args.embedding_size
+
+    tokenizer = text.Tokenizer(num_words=max_features)
+    tokenizer.fit_on_texts(list(X_train) + list(X_test))
+    X_train = tokenizer.texts_to_sequences(X_train)
+    X_test = tokenizer.texts_to_sequences(X_test)
+    x_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+    x_test = sequence.pad_sequences(X_test, maxlen=maxlen)
+
+    print("Loading word embedding ... ")
+    def get_coefs(word, *arr): return word, np.asarray(arr, dtype='float32')
+    embeddings_index = dict(get_coefs(*o.strip().split()) for o in open(EMBEDDING_FILE))
+
+    all_embs = np.stack(embeddings_index.values())
+    emb_mean, emb_std = all_embs.mean(), all_embs.std()
+
+    word_index = tokenizer.word_index
+    nb_words = min(max_features, len(word_index))
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
+    for word, i in word_index.items():
+        if i >= max_features: continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+
+    [X_tra, X_val, y_tra, y_val] = train_test_split(x_train, y_train, train_size=0.95, random_state=233)
+    return X_tra, X_val, y_tra, y_val, embedding_matrix, nb_words
+
 
 class RocAucEvaluation(Callback):
     def __init__(self, validation_data=(), interval=1):
@@ -13,3 +70,28 @@ class RocAucEvaluation(Callback):
             y_pred = self.model.predict(self.X_val, verbose=0)
             score = roc_auc_score(self.y_val, y_pred)
             print("\n ROC-AUC - epoch: %d - score: %.6f \n" % (epoch+1, score))
+
+
+
+def training_callbacks(checkpoint_path, patience, monitor, verbose=2):
+    """ Callbacks for model training.
+
+    # Arguments:
+        checkpoint_path: Where weight checkpoints should be saved.
+        patience: Number of epochs with no improvement after which
+            training will be stopped.
+
+    # Returns:
+        Array with training callbacks that can be passed straight into
+        model.fit() or similar.
+    """
+    cb_verbose = (verbose >= 2)
+    checkpointer = ModelCheckpoint(monitor=monitor, filepath=checkpoint_path,
+                                   save_best_only=True, verbose=cb_verbose)
+    earlystop = EarlyStopping(monitor=monitor, patience=patience,
+                              verbose=cb_verbose)
+
+    ckpt_name, ckpt_ext = os.path.splitext(checkpoint_path)
+    csv_logger = CSVLogger(ckpt_name + ".log")
+
+    return [checkpointer, earlystop, csv_logger]
